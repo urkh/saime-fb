2e3fce33ce92becf5ba380de07a8c171556ac97e
Robert Barrios <arksynth@gmail.com>
2015-02-01 12:48:31 -0430

    ksadjsakdjalskdjalskdja# Please enter the commit message for your changes. Lines starting

--- js/app.js
+++ js/app.js
@@ -152,6 +152,10 @@ var app = angular.module('app', [
                 url: '/registro',
                 templateUrl: 'templates/registro.html'
             })
+            .state('saime.registro_exitoso', {
+                url: '/registro/exitoso',
+                templateUrl: 'templates/registro_exitoso.html'
+            })
 
     }
   ]
