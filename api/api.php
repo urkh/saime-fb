c9ef3ce4877efe291fc24b02635d921ec9347793
urkh <gust989@gmail.com>
2015-02-01 19:52:41 -0430

    mejorando php

--- api/api.php
+++ api/api.php
@@ -3,6 +3,8 @@ include('Requests-1.6.0/library/Requests.php');
 
 Requests::register_autoloader();
 
+$HOST = "http://www.predimania.com:8080";
+
 $data = json_decode(file_get_contents("php://input"));
 $data =  (array) $data;
 extract($data);
@@ -13,14 +15,124 @@ switch ($_GET['opc']) {
         get_login($username, $password);
         break;
 
-    case "get_tramime_detalles":
-        get_tramime_detalles($data);
+    case "get_perfil":
+        get_bearer_auth($URI="/saime-ws/v1.0/me");
         break;
 
-    case "set_nuevo_usuario":
-        echo "set_nuevo_usuario";
+    case "reg_usuario":
+        post_basic_auth($URI="/saime-ws/v1.0/register/insert", $data);
         break;
-        
+
+    case "reg_persona":
+    	post_bearer_auth($URI="/saime-ws/v1.0/passport/saveDataPersonal", $data);
+        break;
+
+    case "sol_ven_menor":
+        post_bearer_auth($URI="/saime-ws/v1.0/passport/saveDataMinor", $data);
+        break;
+
+    case "sol_ext_mayor":
+        post_bearer_auth($URI="/saime-ws/v1.0/passport/saveDataConsularPersonal", $data);
+        break;
+
+    case "sol_ext_menor":
+        post_bearer_auth($URI="/saime-ws/v1.0/passport/saveDataConsularMinor", $data);
+        break;
+
+    case "validar_cita":
+        get_bearer_auth($URI="/saime-ws/v1.0/passport/isValidProcessPersonal");
+        break;
+
+
+    case "modificar_registro":
+        post_bearer_auth($URI="/saime-ws/v1.0/register/update", $data);
+        break;
+
+    case "detalle_tramites":
+        post_bearer_auth($URI="/saime-ws/v1.0/transaction/details", $data);
+        break;
+
+    case "req_pass":
+        post_basic_auth($URI="/saime-ws/v1.0/recover", $data);
+        break;
+
+    case "rechazar_cita":
+        post_bearer_auth($URI="/saime-ws/v1.0/transaction/reject", $data);
+        break;
+
+
+
+
+    case "get_paises":
+        get_bearer_auth($URI="/saime-ws/v1.0/common/countryList", $data);
+        break;
+
+
+    case "get_estados":
+        get_bearer_auth($URI="/saime-ws/v1.0/common/stateList");
+        break;
+
+
+    case "get_municipios":
+        get_bearer_auth($URI="/saime-ws/v1.0/common/townList");
+        break;
+
+
+    case "get_parroquias":
+        get_bearer_auth($URI="/saime-ws/v1.0/common/parishList");
+        break;
+
+
+    case "get_oficinas":
+        get_bearer_auth($URI="/saime-ws/v1.0/common/officeList");
+        break;
+
+
+    case "get_oficina_quota":
+        post_bearer_auth($URI="/saime-ws/v1.0/passport/officeQuota");
+        break;
+
+
+    case "get_oficina_quota_all":
+        post_bearer_auth($URI="/saime-ws/v1.0/passport/officeQuotaAll");
+        break;
+
+
+	case "get_cedula":
+        post_bearer_auth($URI="/saime-ws/v1.0/cedulado", $data_string);
+        break;
+
+
+    case "get_consulados":
+        get_bearer_auth($URI="/saime-ws/v1.0/common/consularList");
+        break;
+
+
+    case "get_estado_tramites":
+        get_bearer_auth($URI="/saime-ws/v1.0/transaction/list");
+        break;
+
+
+    case "get_oficinas_web":
+        get_bearer_auth($URI="/saime-ws/v1.0/portal/oficinas");
+        break;
+
+
+    case "get_noticias_web":
+        get_bearer_auth($URI="/saime-ws/v1.0/portal/noticias");
+        break;
+
+
+    case "get_tramites_web":
+        get_bearer_auth($URI="/saime-ws/v1.0/portal/tramites");
+        break;
+
+
+    case "get_version":
+        get_basic_auth($URI="/saime-ws/v1.0/healthcheck");
+        break;
+
+    
     default:
         echo "ninguno";
 }
@@ -48,53 +160,52 @@ function gen_crc($data){
 // Login
 function get_login($username, $password){
 
-	$url = "http://www.predimania.com:8080/saime-ws/oauth/token?grant_type=password&username=$username&password=$password";
+	$URI = "/saime-ws/oauth/token?grant_type=password&username=$username&password=$password";
 	$headers = array('Content-Type' => 'application/json', 'Authorization' => 'Basic MzUzYjMwMmM0NDU3NGY1NjUwNDU2ODdlNTM0ZTdkNmE6Mjg2OTI0Njk3ZTYxNWE2NzJhNjQ2YTQ5MzU0NTY0NmM=');
-	$response = Requests::post($url, $headers);
+	$response = Requests::post($HOST+$URI, $headers);
 	echo $response->body;
 
 }
 
 
-// Obtener los datos del perfil del ciudadano
-function get_perfil(){
-	$url = "http://www.predimania.com:8080/saime-ws/v1.0/me";
+function get_bearer_auth($URI){
+
 	$headers = array('Content-Type' => 'application/json', 'Authorization' => 'Bearer <access_token>');
-	$response = Requests::get($url, $headers);
+	$response = Requests::get($HOST.$URI, $headers);
 	var_dump($response->body);
 
 }
 
-// Nuevo Usuario
-function set_nuevo_usuario($datos){
 
-	$url = "http://www.predimania.com:8080/saime-ws/v1.0/register/insert";
+function post_bearer_auth($URI, $datos){
+	
+	$headers = array('Content-Type' => 'application/json', 'Authorization' => 'Bearer <access_token>');
+	$data = array('data' => parser_datos($datos), 'crc'=> gen_crc(parser_datos($datos)));
+	$response = Requests::post($HOST.$URI, $headers, json_encode($data));
+	var_dump($response->body);
+
+}
+
+
+
+function get_basic_auth($URI, $datos){
+
+	$headers = array('Content-Type' => 'application/json', 'Authorization' => 'Basic MzUzYjMwMmM0NDU3NGY1NjUwNDU2ODdlNTM0ZTdkNmE6Mjg2OTI0Njk3ZTYxNWE2NzJhNjQ2YTQ5MzU0NTY0NmM=');
+	$response = Requests::get($HOST.$URI, $headers);
+	var_dump($response->body);
+
+}
+
+
+
+function post_basic_auth($URI, $datos){
+
 	$headers = array('Content-Type' => 'application/json', 'Authorization' => 'Basic MzUzYjMwMmM0NDU3NGY1NjUwNDU2ODdlNTM0ZTdkNmE6Mjg2OTI0Njk3ZTYxNWE2NzJhNjQ2YTQ5MzU0NTY0NmM=');
 	$data = array('data'=> parser_datos($datos), 'crc'=> gen_crc(parser_datos($datos)));
-	$response = Requests::post($url, $headers, json_encode($data));
+	$response = Requests::post($HOST.$URI, $headers, json_encode($data));
 	var_dump($response->body);
 
 }
-
-
-// Detalles del tramite
-function get_tramime_detalles($datos){
-	
-	$url = 'http://www.predimania.com:8080/saime-ws/v1.0/transaction/details';
-	$headers = array('Content-Type' => 'application/json', 'Authorization' => 'Bearer 406455a4-d7c2-44cd-9f56-777ef54c2679');
-	$data = array('data' => parser_datos($datos), 'crc'=> gen_crc(parser_datos($datos)));
-	$response = Requests::post($url, $headers, json_encode($data));
-	var_dump($response->body);
-	//$response['tramiteCritico'];
-	//foreach ($response as $dato) {
-	//	echo $dato['tramiteCritico'];
-	//}
-
-	//echo $data['crc'];
-}
-
-
-
 
 
 ?>
